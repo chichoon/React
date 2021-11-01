7493bf30ea19c0b5f43b24b041bf6067d933dbea
chichoon <chichoon.choi@gmail.com>
2021-11-01 20:18:03 +0900

    [react-webgame] ADD: 2-3 wordRelay.jsx 추가

---
+++ react_web_game/2-3/wordRelay.jsx
@@ -0,0 +1,10 @@
+const React = require("react"); //쓰이는 패키지 라이브러리 추가하기
+const { Component } = React;
+
+class WordRelay extends Component {
+  state = {};
+
+  render() {}
+}
+
+module.exports = WordRelay;
