e6a650d3c8207e74eea274be523fdccb96012195
chichoon <chichoon.choi@gmail.com>
2021-11-01 20:21:49 +0900

    [react-webgame] UPDATE: client.jsx, config.js 업데이트

--- react_web_game/2-3/client.jsx
+++ react_web_game/2-3/client.jsx
@@ -1,4 +1,6 @@
 const React = require("react");
 const ReactDom = require("react-dom");
 
-ReactDom.render();
+const WordRelay = require("./WordRelay"); //모듈 불러오는 부분
+
+ReactDom.render(<WordRelay />, document.querySelector("#root"));
--- react_web_game/2-3/webpack.config.js
+++ react_web_game/2-3/webpack.config.js
@@ -1,1 +1,22 @@
-module.exports = {};
+const path = require("path"); //path 라이브러리 불러오기
+//node 설치했다면 path는 자동으로 설치된다
+
+module.exports = {
+  name: "wordrelay-setting", //설정의 이름
+  mode: "development", //설정의 모드 (실 서비스에서는 production으로, 개발중에는 development)
+  devtool: "eval", //툴 속도 빠르게
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  //중요한 애들
+  entry: {
+    //입력파일
+    app: ["./client"],
+  },
+  output: {
+    //출력파일
+    path: path.join(__dirname, "dist"), //__dirname은 현재 경로
+    filename: "app.js",
+  },
+};
