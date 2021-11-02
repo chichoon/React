c9af66b7ba189d856156ebaefa2df14590851a52
chichoon <chichoon.choi@gmail.com>
2021-11-02 23:57:12 +0900

    [react-webgame] UPDATE: 생성된 app.js와 config 파일 업데이트

--- react_web_game/2-3/webpack.config.js
+++ react_web_game/2-3/webpack.config.js
@@ -14,6 +14,20 @@ module.exports = {
     //입력파일
     app: ["./client"],
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/, //js 파일과 jsx 파일에 이 룰을 적용할 것이라는 뜻
+        //정규표현식에 의하면, .js까지는 무조건 매치되어야 하고 x는 x가 존재하거나 존재하지 않는 경우만 판단
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+    ],
+  },
+
   output: {
     //출력파일
     path: path.join(__dirname, "dist"), //__dirname은 현재 경로
